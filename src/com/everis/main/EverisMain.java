02d3d7d63e71025e2080274cdb9f5e7e2e5defc8
Everis Formación <everis.formacion.svq@gmail.com>
2021-02-04 16:55:52 +0100

    GIT - Proyecto 2 - Base

---
+++ src/com/everis/main/EverisMain.java
@@ -0,0 +1,24 @@
+package com.everis.main;
+
+/**
+ * Formación - GIT
+ * 
+ * @author Everis
+ *
+ */
+public class EverisMain {
+
+	/**
+	 * Método principal.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hola a tod@s");
+		
+		/* INTRODUCIR NUEVOS SALUDOS */
+
+	}
+
+}
